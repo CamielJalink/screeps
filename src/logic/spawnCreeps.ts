b393aa10cced02ed85f4fc8493b33f7e28d6f5c9
Camiel Jalink <camiel.jalink@gmail.com>
2020-11-01 14:32:07 +0100

    Spawn now correctly considers the energy of extensions when determining when to build a new creep

--- src/logic/spawnCreeps.ts
+++ src/logic/spawnCreeps.ts
@@ -8,14 +8,28 @@ export default function spawnCreeps(){
 
 function checkIfShouldSpawn(homeSpawn: StructureSpawn){
 
-  let spawnEnergy = homeSpawn.store.getUsedCapacity("energy");
-  let creeps: Creep[] = homeSpawn.room.find(FIND_MY_CREEPS);
+  let extensionEnergy: number = 0;
+  homeSpawn.room.find(FIND_MY_STRUCTURES).forEach((structure) =>{
+    if(structure.structureType == "extension"){
+      extensionEnergy += structure.store.getUsedCapacity("energy");
+    }
+  })
+  let spawnEnergy = extensionEnergy + homeSpawn.store.getUsedCapacity("energy");
+  console.log(spawnEnergy);
 
+  let creeps: Creep[] = homeSpawn.room.find(FIND_MY_CREEPS);
   let harvesterCreeps: Creep[] = creeps.filter((creep) => creep.memory.role == 'harvester');
   let upgraderCreeps: Creep[] = creeps.filter((creep) => creep.memory.role == 'upgrader');
   let builderCreeps: Creep[] = creeps.filter((creep) => creep.memory.role == 'builder');
 
-  if (spawnEnergy > 200 && upgraderCreeps.length < 4) {
+  if (spawnEnergy >= 200 && harvesterCreeps.length < 4) {
+    let name = 'Harvester' + Memory.creepCounters.harvesterCounter;
+    homeSpawn.spawnCreep([MOVE, CARRY, WORK], name, { memory: { role: 'harvester' } })
+    Memory.creepCounters.harvesterCounter = Memory.creepCounters.harvesterCounter + 1;
+    return;
+  }
+
+  else if (spawnEnergy >= 200 && upgraderCreeps.length < 4) {
     let name = 'Upgrader' + Memory.creepCounters.upgraderCounter;
     let memory = {
       role: 'upgrader',
@@ -25,13 +39,8 @@ function checkIfShouldSpawn(homeSpawn: StructureSpawn){
     Memory.creepCounters.upgraderCounter = Memory.creepCounters.upgraderCounter + 1;
     return;
   }
-  else if (spawnEnergy > 200 && harvesterCreeps.length < 4) {
-    let name = 'Harvester' + Memory.creepCounters.harvesterCounter;
-    homeSpawn.spawnCreep([MOVE,CARRY,WORK], name, {memory: {role: 'harvester'}})
-    Memory.creepCounters.harvesterCounter = Memory.creepCounters.harvesterCounter + 1;
-    return;
-  }
-  else if (spawnEnergy > 200 && builderCreeps.length < 4){
+
+  else if (spawnEnergy >= 200 && builderCreeps.length < 4){
     let name = 'Builder' + Memory.creepCounters.builderCounter;
     let memory = {
       role: 'builder',
