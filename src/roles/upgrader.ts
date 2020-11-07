1e1ceb4b6849033bfc48cb8eb8a7d371c272679e
Camiel Jalink <camiel.jalink@gmail.com>
2020-11-07 20:54:02 +0100

    Upgrader code refactored to be more like harvester code

--- src/roles/upgrader.ts
+++ src/roles/upgrader.ts
@@ -3,26 +3,26 @@ export default function upgrader(creep: Creep) {
   let sources: Source[] = homeSpawn.room.find(FIND_SOURCES);
 
   if(homeSpawn.room.controller){
-
-    if(creep.memory.upgraderMode == "upgrade"){
-      if (creep.upgradeController(homeSpawn.room.controller) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(homeSpawn.room.controller);
-      }
-      if (creep.store.getUsedCapacity('energy') == 0){
-        creep.memory.upgraderMode = "mine";
-      }
+    if (creep.memory.upgraderMode === "mine" && creep.store.getFreeCapacity("energy") === 0) {
+      creep.memory.upgraderMode = "upgrade";
     }
-    else if(creep.memory.upgraderMode == "mine"){
-      if (creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(sources[1]);
-      };
-      if (creep.store.getFreeCapacity() == 0){
-        creep.memory.upgraderMode = "upgrade";
-      }
-    }
-    else{
+    else if (creep.memory.upgraderMode === "upgrade" && creep.store.getUsedCapacity("energy") === 0) {
       creep.memory.upgraderMode = "mine";
     }
 
+
+    if(creep.memory.upgraderMode === "upgrade"){
+      if (creep.upgradeController(homeSpawn.room.controller) === ERR_NOT_IN_RANGE) {
+        creep.moveTo(homeSpawn.room.controller);
+      }
+    }
+    else if(creep.memory.upgraderMode === "mine"){
+      if (creep.harvest(sources[1]) === ERR_NOT_IN_RANGE) {
+        creep.moveTo(sources[1]);
+      };
+    }
+    else{
+      console.log("no work for upgrader " + creep.name);
+    }
   }
 }
