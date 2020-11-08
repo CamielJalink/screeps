212daf168a0da92abbc5dc7400e7eec6a085c836
Camiel Jalink <camiel.jalink@gmail.com>
2020-11-08 10:06:27 +0100

    Refactored builder logica to be more readable, have better repair logic and have an explicit idle mode

--- src/roles/builder.ts
+++ src/roles/builder.ts
@@ -1,6 +1,8 @@
 export default function builder(creep: Creep) {
   let homeSpawn = Game.spawns['Home'];
+  let constructionSites: ConstructionSite[] = homeSpawn.room.find(FIND_MY_CONSTRUCTION_SITES);
 
+  // Containers to get store energy from
   let containers: Structure[] = [];
   homeSpawn.room.find(FIND_STRUCTURES).forEach((structure) => {
     if (structure.structureType == "container") {
@@ -8,42 +10,50 @@ export default function builder(creep: Creep) {
     };
   })
 
-  let constructionSites: ConstructionSite[] = homeSpawn.room.find(FIND_MY_CONSTRUCTION_SITES);
-  
+  let damagedStructures: Structure[] = [];
+  homeSpawn.room.find(FIND_STRUCTURES).forEach((structure) => {
+    if (structure.hits < structure.hitsMax){
+      damagedStructures.push(structure);
+    };
+  });
 
-  if (creep.store.getFreeCapacity() == 0) {
+
+// If creep has no energy, get some
+  if(creep.store.getUsedCapacity("energy") === 0){
+    creep.memory.builderMode = "withdraw";
+  }
+// If creep has energy and something is damaged, go repair it
+  else if(damagedStructures.length > 0){
+    creep.memory.builderMode = "repair";
+  }
+// If creep has energy, nothing is damaged and there is a construction site, go build!
+  else if(constructionSites.length > 0){
     creep.memory.builderMode = "build";
   }
-  else if(creep.store.getUsedCapacity() == 0){
-    creep.memory.builderMode = 'withdraw';
+// If we have energy and nothing needs doing, lets idle somewhere out of way
+  else{
+    creep.memory.builderMode = "idle";
   }
 
-
-  if(creep.memory.builderMode == "withdraw"){
-    if(creep.withdraw(containers[0], "energy") == ERR_NOT_IN_RANGE){
+  
+  if(creep.memory.builderMode === "withdraw"){
+    if(creep.withdraw(containers[0], "energy") === ERR_NOT_IN_RANGE){
       creep.moveTo(containers[0]);
     }
   }
-  else if(creep.memory.builderMode == "build"){
-    containers.forEach((container) => {
-      if(container.hits < container.hitsMax){
-        if (creep.repair(container) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(container);
-        };
-      }
-    })
-    if(creep.build(constructionSites[0]) == ERR_NOT_IN_RANGE){
+  else if (creep.memory.builderMode === "repair") {
+    if (creep.repair(damagedStructures[0]) === ERR_NOT_IN_RANGE) {
+      creep.moveTo(damagedStructures[0]);
+    }
+  }
+  else if(creep.memory.builderMode === "build"){
+    if(creep.build(constructionSites[0]) === ERR_NOT_IN_RANGE){
       creep.moveTo(constructionSites[0]);
     }
   }
-  else if(constructionSites.length === 0 && creep.memory.builderMode === "build") {
-    creep.moveTo(containers[0]);
-  }
+  else if(creep.memory.builderMode === "idle"){
+    if (creep.withdraw(containers[0], "energy") === ERR_NOT_IN_RANGE) {
+      creep.moveTo(containers[0]);
+    }
+  };
 }
-
-
-// Als ik helemaal leeg ben, ga ik energie halen. 
-// Als ik energie heb:
-//  Als er iets te repareren valt, ga dat repareren. 
-//  Als er iets te bouwen valt, ga dat bouwen. 
-//  Als 
